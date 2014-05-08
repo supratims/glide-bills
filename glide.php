e93f6bd005fe71f6efe5aa9a92641a0aaff07540
m1ke <git@m1ke.me>
2014-05-08 17:26:35 +0100

    Better test coverage for missing items

--- glide.php
+++ glide.php
@@ -1,10 +1,5 @@
 <?php
-
 class Glide {
-	protected $glide_human_name=array("electricity"=>"Electricity","tv_license"=>"TV License");
-
-	private $api_key;
-
 	protected $url='https://www.glide.uk.com/api/4.0/';
 	protected $methods=array('frontEnd'=>array(
 	),'portal'=>array(
@@ -20,8 +15,12 @@ class Glide {
 			'email'=>true,
 		),
 	),);
-	protected $services=array('gas','electricity','water','telephone','broadband','tv');
+	protected $service_names=array('gas'=>'Gas','electricity'=>'Electricity','water'=>'Water','telephone'=>'Phone','broadband'=>'Internet','tv'=>'TV license');
 	protected $html_error=array();
+
+	private $api_key;
+	private $services;
+	private $postcode_no_water=false;
 
 	public $log_html=true;
 	public $log_dir='logs';
@@ -36,33 +35,12 @@ class Glide {
 			throw Exception('You must set a valid Glide API key.');
 		}
 		$this->api_key=$api_key;
+		$this->services=array_keys($this->service_names);
 		return $this;
 	}
 
-	function get_quote(Array $services=array('electricity'),Array $extra=array(),$period=6,$tenants=1,$postcode='m1 1dz'){
-		$quotes=$errors=array();
-		foreach ($services as $service){
-			try {
-				$quotes[$service]=$this->get_service_quote($service,$extra[$service],$period,$tenants,$postcode);
-			}
-			catch (Exception $e){
-				$errors[]=$e->getMessage();
-			}
-			// $quotes[$service]["human_name"]=$this->insert_human_name($service);
-		}
-		if (!empty($errors)){
-			foreach ($this->html_error as $n => $html){
-				if (!empty($html)){
-					$this->_file_save($this->log_dir.'/glide_error'.$n.'.html',$html,true);
-					$saved=true;
-				}
-			}
-			throw new Exception('There was an error with the Glide API.'.($saved ? ' Log files have been saved at "'.$this->log_dir.'/"' : '').' More details: '.PHP_EOL.implode(PHP_EOL,$errors));
-		}
-		return $this->calculate_totals($quotes);
-	}
-
 	function set_postcode($postcode){
+		$this->postcode_no_water=false;
 		$this->postcode=$postcode;
 		return $this;
 	}
@@ -79,13 +57,14 @@ class Glide {
 
 	function set_broadbandType($broadbandType){
 		if (!in_array($broadbandType,$this->broadbandTypes)){
-			throw new Exception('You must choose a valid broadband type.');
+			throw new GlideValidException('You must choose a valid broadband type.');
 		}
 		$this->broadbandType=$broadbandType;
 		return $this;
 	}
 
-	function signUp_quote_allServices($no_water=false){
+	function signUp_quote_allServices(){
+		$this->valid_signUp_quote();
 		$data=array(
 			'postcode'=>$this->postcode,
 			'capacity'=>$this->tenants,
@@ -94,7 +73,7 @@ class Glide {
 		foreach ($this->services as $service){
 			$data[$service]=true;
 		}
-		if ($no_water){
+		if ($this->postcode_no_water){
 			unset($data['water']);
 		}
 		if (!empty($this->broadbandType)){
@@ -106,32 +85,51 @@ class Glide {
 		}
 		$res=$this->send_request($data,'signUp/quote/allServices');
 		if ($res['error']==1){
-			if (!$no_water and strpos($res['message'],'We are unable to supply water')!==false){
-				$res=$this->signUp_quote_allServices(true);
+			if (!$this->postcode_no_water and strpos($res['message'],'We are unable to supply water')!==false){
+				$this->postcode_no_water=true;
+				$res=$this->signUp_quote_allServices();
+			}
+			else {
+				$this->exception_message($res);
 			}
 		}
 		return $res;
 	}
 
+	private function exception_no_water(){
+		throw new GlideException('It is not possible to get a water quote for this postcode.');
+	}
+
+	private function exception_message($res){
+		throw new GlideException('The Glide server reported an error: '.$res['message']);
+	}
+
 	function signUp_quote_servicePrice($service){
+		$this->valid_signUp_quote();
 		$data=array(
 			'postcode'=>$this->postcode,
 			'capacity'=>$this->tenants,
 			'minTerm'=>$this->term,
 		);
 		if (!in_array($service,$this->services)){
-			throw new Exception('The selected service ('.$service.') is not valid.');
+			throw new GlideValidException('The selected service ('.$service.') is not valid.');
 		}
 		$data[$service]=true;
+		if ($service=='water' and $this->postcode_no_water){
+			$this->exception_no_water();
+		}
 		if ($service=='broadband'){
 			if (empty($this->broadbandType)){
-				throw new Exception('You must enter a broadband type to check broadband prices.');
+				throw new GlideValidException('You must enter a broadband type to check broadband prices.');
 			}
 			$data['broadbandType']=$this->broadbandType;
 		}
 		$res=$this->send_request($data,'signUp/quote/allServices');
 		if ($res['error']==1){
-
+			if (!$this->postcode_no_water and strpos($res['message'],'We are unable to supply water')!==false){
+				$this->exception_no_water();
+			}
+			$this->exception_message($res);
 		}
 		return $res;
 	}
@@ -147,11 +145,11 @@ class Glide {
 			$errors[]='No term.';
 		}
 		if (!empty($errors)){
-			throw new Exception('The following data input errors have occured:'."<br/>\n".implode("<br/>\n",$errors));
+			throw new GlideValidException('The following data input errors have occured:'."<br/>\n".implode("<br/>\n",$errors));
 		}
 	}
 
-	private function send_request($data,$route){
+	private function send_request(Array $data,$route){
 		if ($route[0]=='/'){
 			$route=substr($route,1);
 		}
@@ -159,7 +157,7 @@ class Glide {
 		$methods=$this->methods;
 		foreach ($check_route as $step){
 			if (!isset($methods[$step])){
-				throw new Exception('The route '.$route.' could not be found. It failed looking for "'.$step.'"');
+				throw new GlideValidException('The route '.$route.' could not be found. It failed looking for "'.$step.'"');
 			}
 			$methods=$methods[$step];
 		}
@@ -172,11 +170,11 @@ class Glide {
 		$res_arr=json_decode($res,true);
 		if ($res_arr===false or $res_arr===null){
 			if (empty($res)){
-				throw new Exception('No data was returned from '.$url.'.');
+				throw new GlideException('No data was returned from '.$url.'.');
 			}
 			else {
 				$this->_file_save($this->log_dir.'/glide_error.html',$res,true);
-				throw new Exception('The JSON data could not be parsed from '.$url.'.');
+				throw new GlideException('The JSON data could not be parsed from '.$url.'.');
 			}
 		}
 		return $res_arr;
@@ -184,15 +182,6 @@ class Glide {
 
 	private function insert_human_name($service){
 		return $this->glide_human_name[$service] || ucfirst($service);
-	}
-
-	private function calculate_totals($quotes){
-		$quotes["total"] = array();
-		$build=array("tenant_week", "tenant_month", "monthly_fee");
-		foreach ($build as $k){
-			// $quotes["total"][$k] = "%.2f" % quotes.map { |e| e[1][k].to_f }.reduce(:+)
-		}
-		return $quotes;
 	}
 
 	// from http://www.lornajane.net/posts/2011/posting-json-data-with-php-curl
@@ -221,4 +210,12 @@ class Glide {
 			return false;
 		}
 	}
+}
+
+class GlideValidException extends Exception {
+	
+}
+
+class GlideException extends Exception {
+
 }
--- test.php
+++ test.php
@@ -8,21 +8,54 @@ class testGlide extends PHPUnit_Framework_TestCase {
 	private $term=12;
 
 	function __construct(){
+		$this->glide=$this->glide_new();
+	}
+
+	private function glide_new(){
 		$api_key=shell_exec('cat api_key');
 		if (empty($api_key)){
 			throw new Exception('Create a file called "api_key" which constains your Glide API key.');
 		}
-		$glide=new Glide($api_key);
-		$this->glide=$glide->set_postcode($this->postcode)->set_tenants($this->tenants)->set_term($this->term);
+		return new Glide($api_key);
 	}
 
-	function testGlideQuoteServices(){
-		print_r($this->glide->signUp_quote_allServices());
-		$this->assertTrue(true);
+	private function quote_setup(){
+		$this->glide->set_postcode($this->postcode)->set_tenants($this->tenants)->set_term($this->term);
 	}
 
-	function testGlideQuoteService(){
-		print_r($this->glide->signUp_quote_servicePrice('electricity'));
-		$this->assertTrue(true);
+	function testGlideQuoteServicesReturnsArray(){
+		$this->quote_setup();
+		$res=$this->glide->signUp_quote_allServices();
+		$this->assertTrue(is_array($res));
 	}
+
+	function testGlideQuoteServiceReturnsArray(){
+		$this->quote_setup();
+		$res=$this->glide->signUp_quote_servicePrice('electricity');
+		$this->assertTrue(is_array($res));
+	}
+
+	function testGlideExceptionNoPostcode(){
+		$this->setExpectedException('GlideValidException');
+		$this->glide->set_tenants($this->tenants)->set_term($this->term)->signUp_quote_allServices();
+	}
+
+	function testGlideExceptionNoTerm(){
+		$this->setExpectedException('GlideValidException');
+		$this->glide->set_postcode($this->postcode)->set_tenants($this->tenants)->signUp_quote_allServices();
+	}
+
+	function testGlideExceptionNoTenants(){
+		$this->setExpectedException('GlideValidException');
+		$this->glide->set_postcode($this->postcode)->set_term($this->term)->signUp_quote_allServices();
+	}
+
+	function testGlideRouteFailure(){
+        $method=new ReflectionMethod('Glide','send_request');
+ 
+        $method->setAccessible(TRUE);
+		$this->setExpectedException('GlideValidException');
+ 
+        $method->invoke($this->glide_new(),array(),'made/up/route');
+    }
 }
