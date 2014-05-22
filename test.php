87d6a46a12033ad393a9917e8665f3b8b35f0746
m1ke <git@m1ke.me>
2014-05-22 11:54:35 +0100

    Clean up

--- test.php
+++ test.php
@@ -43,7 +43,6 @@ class testGlide extends PHPUnit_Framework_TestCase {
 
     function testGlideQuoteServicesAllReturnsArray(){
     	$res=$this->glide->signUp_quote_allServices($this->service_data,array('all'=>true));
-    	var_dump($res);
     	$this->assertTrue(is_numeric($res['gas']['monthly_fee']));
     	$this->assertTrue(is_numeric($res['electricity']['monthly_fee']));
     	$this->assertTrue(is_numeric($res['telephone']['monthly_fee']));
